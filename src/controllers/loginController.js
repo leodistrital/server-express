292efd70234c68de17d924bd70810f3331238232
Leonardo Cortes <leodistrital@hotmail.com>
2023-11-16 03:31:55 -0500

    mejorando mensajes de error

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,28 +1,25 @@
-const connection = require('../models/db')
-const jwt = require('jsonwebtoken');
+const connection = require("../models/db");
+const jwt = require("jsonwebtoken");
 
-module.exports.login = (req, res) =>{
-    const {username, password} = req.body;
-    const consult = 'SELECT * FROM login WHERE username = ? AND password = ?';
+module.exports.login = (req, res) => {
+	const { username, password } = req.body;
+	const consult = "SELECT * FROM login WHERE username = ? AND password = ?";
 
-    try {
-      connection.query(consult, [username, password], (err, result)=>{
-          if(err){
-              res.send(err);
-          }
+	try {
+		connection.query(consult, [username, password], (err, result) => {
+			if (err) {
+				res.send(err);
+			}
 
-          if(result.length > 0){
-              const token = jwt.sign({username}, "Stack", {
-                  expiresIn: '3m'
-              });
-              res.send({token});
-          } else {
-              console.log('wrong user')
-              res.send({message: 'wrong user'})
-          }
-      })
-    } catch (e) {
-
-    }
-
-}
+			if (result.length > 0) {
+				const token = jwt.sign({ username }, "Stack", {
+					expiresIn: "3m",
+				});
+				res.send({ token, login: true });
+			} else {
+				console.log("wrong user");
+				res.send({ message: "wrong user", login: false });
+			}
+		});
+	} catch (e) {}
+};
